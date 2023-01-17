94c3a7f72af1a8fdaf056eb547e0713abbce6162
ivan <bochenkoivan@gmail.com>
2023-01-18 00:08:16 +0200

    fix type error

--- env.ts
+++ env.ts
@@ -6,7 +6,7 @@ const requiredServerEnvs = [
 	'AWS_SECRET_ACCESS_KEY',
 	'FACEBOOK_CLIENT_ID',
 	'FACEBOOK_CLIENT_SECRET',
-	'EXPO_SLUG',
+	'EXPO_SLUG'
 ] as const
 
 type RequiredServerEnvKeys = (typeof requiredServerEnvs)[number]
