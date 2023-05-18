29d1cd4b39958aa7dadc6d59415b9bca85532bfb
ivan <bochenkoivan@gmail.com>
2023-05-18 13:23:13 +0300

    d

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import loginRouter from './routes/login'
 import devRouter from './routes/dev'
 import { uploadToS3 } from './utils/upload'
 import { verifyToken } from "./utils/token"
+import bodyParser from "body-parser"
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -16,7 +17,7 @@ const upload = multer({ storage })
 
 app.use(cors())
 app.use(express.json())
-app.use(express.raw({ type: "application/vnd.custom-type" }))
+app.use(bodyParser.raw());
 app.use(express.text({ type: "text/html" }))
 
 if(process.env.NODE_ENV === 'dev') {
@@ -43,10 +44,9 @@ if(process.env.NODE_ENV === 'dev') {
 app.use('/graphql', graphQLServer)
 app.use('/login', loginRouter)
 
-app.post('/images', express.raw({type: "*/*"}), async (req, res) => {
+app.post('/images', upload.single('image'), async (req, res) => {
   const user_id = app.get('user_id')
-  console.log(req.body)
-  const file = req.body
+  const { file } = req
   if (!file || !user_id) return res.status(400).json({ message: "Bad request" })
 
   const key = await uploadToS3(file, user_id)
